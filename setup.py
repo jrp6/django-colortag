eda37b7c385ae7180f220aaf13359912c6569092
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-09-14 10:26:06 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-colortag',
-    version='1.0.0',
+    version='1.0.1',
     description='Django tools for data tagging models',
     long_description=long_description,
     keywords='django models tagging',
